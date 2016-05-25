f08500c249fe26c3ea15b8f4e9aae4310f2f501e
JP <jprinz@conestogac.on.ca>
2016-05-25 18:36:25 -0400

    Finished Math Game from chapters 2 - 4.

--- app/src/main/java/com/johnprinz/mathgamechapter2/GameActivity.java
+++ app/src/main/java/com/johnprinz/mathgamechapter2/GameActivity.java
@@ -7,6 +7,8 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.Random;
+
 
 public class GameActivity extends Activity implements View.OnClickListener {
 
@@ -14,37 +16,48 @@ public class GameActivity extends Activity implements View.OnClickListener {
     Button buttonObjectChoice1;
     Button buttonObjectChoice2;
     Button buttonObjectChoice3;
+    TextView textObjectPartA;
+    TextView textObjectPartB;
+    TextView textObjectScore;
+    TextView textObjectLevel;
+
+    int currentScore = 0;
+    int currentLevel = 1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
 
-        int partA = 9;
+        /*int partA = 9;
         int partB = 9;
         correctAnswer = partA * partB;
         int wrongAnswer1 = correctAnswer - 1;
-        int wrongAnswer2 = correctAnswer + 1;
+        int wrongAnswer2 = correctAnswer + 1;*/
 
-        TextView textObjectPartA = (TextView)findViewById(R.id.textPartA);
-        TextView textObjectPartB = (TextView)findViewById(R.id.textPartB);
+        textObjectPartA = (TextView)findViewById(R.id.textPartA);
+        textObjectPartB = (TextView)findViewById(R.id.textPartB);
+
+        textObjectScore = (TextView)findViewById(R.id.textScore);
+        textObjectLevel = (TextView)findViewById(R.id.textLevel);
 
         buttonObjectChoice1 = (Button)findViewById(R.id.buttonChoice1);
         buttonObjectChoice2 = (Button)findViewById(R.id.buttonChoice2);
         buttonObjectChoice3 = (Button)findViewById(R.id.buttonChoice3);
 
-        textObjectPartA.setText("" + partA);
-        textObjectPartB.setText("" + partB);
+        /*textObjectPartA.setText("" + partA);
+        textObjectPartB.setText("" + partB);*/
 
-        buttonObjectChoice1.setText("" + correctAnswer);
+        /*buttonObjectChoice1.setText("" + correctAnswer);
         buttonObjectChoice2.setText("" + wrongAnswer1);
-        buttonObjectChoice3.setText("" + wrongAnswer2);
+        buttonObjectChoice3.setText("" + wrongAnswer2);*/
 
         buttonObjectChoice1.setOnClickListener(this);
         buttonObjectChoice2.setOnClickListener(this);
         buttonObjectChoice3.setOnClickListener(this);
 
-
+        setQuestion();
 
     }
 
@@ -58,37 +71,114 @@ public class GameActivity extends Activity implements View.OnClickListener {
             case R.id.buttonChoice1:
 
                 answerGiven = Integer.parseInt("" + buttonObjectChoice1.getText());
-                if(answerGiven == correctAnswer){
+                /*if(answerGiven == correctAnswer){
                     Toast.makeText(getApplicationContext(), "Well done!", Toast.LENGTH_LONG).show();
                 } else {
                     Toast.makeText(getApplicationContext(), "Sorry that's wrong", Toast.LENGTH_LONG).show();
-                }
+                }*/
 
                 break;
 
             case R.id.buttonChoice2:
 
                 answerGiven = Integer.parseInt("" + buttonObjectChoice2.getText());
-                if(answerGiven == correctAnswer){
+                /*if(answerGiven == correctAnswer){
                     Toast.makeText(getApplicationContext(), "Well done!", Toast.LENGTH_LONG).show();
                 } else {
                     Toast.makeText(getApplicationContext(), "Sorry that's wrong", Toast.LENGTH_LONG).show();
-                }
+                }*/
 
                 break;
 
             case R.id.buttonChoice3:
 
                 answerGiven = Integer.parseInt("" + buttonObjectChoice3.getText());
-                if(answerGiven == correctAnswer){
+                /*if(answerGiven == correctAnswer){
                     Toast.makeText(getApplicationContext(), "Well done!", Toast.LENGTH_LONG).show();
                 } else {
                     Toast.makeText(getApplicationContext(), "Sorry that's wrong", Toast.LENGTH_LONG).show();
-                }
+                }*/
 
                 break;
 
         }
+
+        updateScoreAndLevel(answerGiven);
+        setQuestion();
+
+    }
+
+    void setQuestion(){
+        int numberRange = currentLevel * 3;
+        Random randInt = new Random();
+
+        int partA = randInt.nextInt(numberRange);
+        partA++;
+
+        int partB = randInt.nextInt(numberRange);
+        partB++;
+
+        correctAnswer = partA * partB;
+        int wrongAnswer1 = correctAnswer - 2;
+        int wrongAnswer2 = correctAnswer + 2;
+
+        textObjectPartA.setText("" + partA);
+        textObjectPartB.setText("" + partB);
+
+        int buttonLayout = randInt.nextInt(3);
+        switch (buttonLayout){
+
+            case 0:
+                buttonObjectChoice1.setText("" + correctAnswer);
+                buttonObjectChoice2.setText("" + wrongAnswer1);
+                buttonObjectChoice3.setText("" + wrongAnswer2);
+                break;
+
+            case 1:
+                buttonObjectChoice2.setText("" + correctAnswer);
+                buttonObjectChoice3.setText("" + wrongAnswer1);
+                buttonObjectChoice1.setText("" + wrongAnswer2);
+                break;
+
+            case 2:
+                buttonObjectChoice3.setText("" + correctAnswer);
+                buttonObjectChoice1.setText("" + wrongAnswer1);
+                buttonObjectChoice2.setText("" + wrongAnswer2);
+                break;
+
+        }
+
+    }
+
+    void updateScoreAndLevel(int answerGiven){
+
+        if(isCorrect(answerGiven)){
+            for(int i = 1; i <= currentLevel; i++){
+                currentScore = currentScore + 1;
+            }
+            currentLevel++;
+        }else{
+            currentScore = 0;
+            currentLevel = 1;
+        }
+
+        textObjectScore.setText("Score: " + currentScore);
+        textObjectLevel.setText("Level: " + currentLevel);
+
+    }
+
+
+    boolean isCorrect(int answerGiven){
+        boolean correctTrueOrFalse;
+        if(answerGiven == correctAnswer){
+            Toast.makeText(getApplicationContext(), "Well dont!", Toast.LENGTH_LONG).show();
+            correctTrueOrFalse = true;
+        }else{
+            Toast.makeText(getApplicationContext(), "Sorry", Toast.LENGTH_LONG).show();
+            correctTrueOrFalse = false;
+        }
+
+        return correctTrueOrFalse;
 
     }
 
